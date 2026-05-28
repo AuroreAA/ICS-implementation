ab815cbc5dadd58a338d1f01885eaa7a40b529e5
AuroreAA <aurore.archimbaud@live.fr>
2026-05-28 08:17:32 +0200

    clean repo

--- Julia/Julia_ICS_tutorial.R
+++
@@ -1,6 +0,0 @@
-## ----setup, include=FALSE-----------------------------------------------------
-knitr::knit_hooks$set(purl = knitr::hook_purl)
-knitr::opts_chunk$set(echo = TRUE, eval = TRUE)
-library(JuliaCall)
-julia_setup()
-
